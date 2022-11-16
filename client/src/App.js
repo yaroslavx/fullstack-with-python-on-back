7d5aabf7d032431821b4c1f782717a90fa27c17a
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-11-16 16:18:00 +0300

    add dockerfiles

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const data = await axios.get('http://api:8000/api');
+
+      setUsers(data.data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            {user.id}
+            {user.name}
+            {user.age}
+          </div>
+        );
+      })}
+    </>
   );
 }
 
